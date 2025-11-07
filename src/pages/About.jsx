2c9cdd231e021deb831c5918d6e6c0418b039a58
Lukaszvdblink <9025326@student.zadkine.nl>
2025-11-07 09:22:15 +0100

    Navigatie

---
+++ src/pages/About.jsx
@@ -0,0 +1,10 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About me</h1>
+      <p>Hallo</p>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,10 @@
+const Home = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>Welkom op mijn portfolio website</p>
+    </div>
+  );
+};
+
+export default Home;
